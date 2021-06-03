3f0c9dd69469d39bf1ad4c6f2acfbc47f6aadc14
Kirti Chaturvedi <kirti@Kirtis-MacBook-Pro.local>
2021-06-04 03:12:03 +0530

    started with the to do list task

--- Ecell/Ecell/settings.py
+++ Ecell/Ecell/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'django.contrib.admin',
     'Register',
+    'tasks',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
